498c12e0b609ca04b4ad5a6f719b8f50591edf0f
PeterHarje <peter.harje@live.se>
2026-01-31 01:20:57 +0100

    Release v1.0.0 (ImgNum format v1)

--- obj/Release/net9.0/ImgNum.AssemblyInfo.cs
+++ obj/Release/net9.0/ImgNum.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImgNum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9c726ce26689dc67f7894fcd076450795edde9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImgNum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImgNum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
